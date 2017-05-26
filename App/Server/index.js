f76f0eec07d5d1fac56f12cf0b0390e89b595465
satowine <satowine@gmail.com>
2017-05-26 18:10:25 +0200

    Aggiunto modulo crypt
    -Aggiunto forge e utility crypt e decrypt

--- App/Server/index.js
+++ App/Server/index.js
@@ -1,6 +1,9 @@
 //moustache
 const mu = require('./utility/parser');
 
+//forge
+var encr = require('./utility/encrypt');
+
 //express
 const express = require('express');
 var bodyParser = require('body-parser');
@@ -9,12 +12,22 @@ const app = express();
 //port
 const port = 3000;
 
-app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.post("/salvatore", function(req, res){
+  var myBytes = req.body;
+  var encripted = encr.encrypt(myBytes);
+  encr.decrypt(encripted);
+  res.send("critta e decritta porco dio!");
+})
+
+/*app.use(bodyParser.json());
 app.post("/salvatore", function(req,res) {
   var myMu = req.body;
   mu.parse(myMu);
   res.send("madonna ladra");
-})
+})*/
+
+
 
 app.listen(port, ()=>{
   console.log("SERVER WORKS!!!" + port);
